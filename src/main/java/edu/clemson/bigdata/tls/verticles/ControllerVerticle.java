a418737dfb99ed48ee687e0ae5e107be1a8cb1c5
Pengfei Xuan <pfxuan@gmail.com>
2016-07-08 20:41:34 -0400

    Upgrade Vertx version

--- src/main/java/edu/clemson/bigdata/tls/verticles/ControllerVerticle.java
+++ src/main/java/edu/clemson/bigdata/tls/verticles/ControllerVerticle.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 /**
  * This verticle is used to run TLS-Controller.
  */
-public class MyFirstVerticle extends AbstractVerticle {
+public class ControllerVerticle extends AbstractVerticle {
   //private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(MyFirstVerticle.class);
 
   /**
